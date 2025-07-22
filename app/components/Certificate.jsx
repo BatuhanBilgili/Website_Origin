e33734f5d960fa0de21829ec64768ad89699ad35
Batuhan_Bilgili <91786686+BatuhanBilgili@users.noreply.github.com>
2025-07-22 17:55:20 +0300

    Update Certificate.jsx

--- app/components/Certificate.jsx
+++ app/components/Certificate.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { assets } from '@/assets/assets';
+import { assets } from '../../assets/assets'
 
 const Certificate = ({ isDarkMode }) => {
   const [certificates, setCertificates] = useState([]);
